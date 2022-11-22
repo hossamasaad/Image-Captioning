c0da411e9a8088a9566e74468fd342dd8a512163
hossamasaad <hossamasaad10@gmail.com>
2022-11-22 22:39:32 +0200

    Download data and unzip it to data/raw

---
+++ src/data/download_dataset.py
@@ -0,0 +1,16 @@
+# Download flicker30k dataset from kaggle
+"""
+!pip install -q kaggle
+
+!mkdir -p ~/.kaggle
+!cp kaggle.json ~/.kaggle/
+
+!chmod 600 /root/.kaggle/kaggle.json
+
+!kaggle datasets download -d hsankesara/flickr-image-dataset
+"""
+
+# Unzip dataset to "Image Captioning/data/raw"
+"""
+!unzip flickr-image-dataset.zip -d "Image Captioning/data/raw"
+"""
